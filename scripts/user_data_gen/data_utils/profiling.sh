2198054d8ccccc12e270b5b5b45f51398e127fd7
kimiko <kinqsleyy@gmail.com>
2024-08-07 16:38:28 +0000

    combined both profiling .sh scripts into one, adding flags to specify what to test

--- scripts/user_data_gen/data_utils/profiling.sh
+++ scripts/user_data_gen/data_utils/profiling.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-# defining the number of users to generate
+# ensures correct number of arguments.
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <implementation>"
+    exit 1
+fi
+
+IMPLEMENTATION=$1
+
 NUM_USERS=10000
 
-# defining output file names
-PROFILE_FILE="/home/kimiko/spotify_interface_data_pipeline/artifacts/profiling/data_utils_profile.prof"
-TEXT_FILE="/home/kimiko/spotify_interface_data_pipeline/artifacts/profiling/data_utils_profile.txt"
+DATE_TIME=$(date "+%Y-%m-%d_%H-%M-%S")
+
+PROFILE_FILE="/home/kimiko/spotify_interface_data_pipeline/artifacts/profiling/user_data_gen/data_utils/${DATE_TIME}-data_utils_${IMPLEMENTATION}_profile.prof"
+TEXT_FILE="/home/kimiko/spotify_interface_data_pipeline/artifacts/profiling/user_data_gen/data_utils/${DATE_TIME}-data_utils_${IMPLEMENTATION}_profile.txt"
 
 export PYTHONPATH=/home/kimiko/spotify_interface_data_pipeline
 
-# run the Python profiling command using a here-doc
+# profile the data_utils python script(s). this one script is able to generate profiles for the new and old data
 python3 - <<EOF
 import cProfile
 import pstats
 
-from src.main.python.user_data_gen.data_utils import generate_user_data
 
+# dynamically import the appropriate module based on implementation
+if "${IMPLEMENTATION}" == "old":
+    from src.main.python.user_data_gen.data_utils_old import generate_user_data
+elif "${IMPLEMENTATION}" == "new":
+    from src.main.python.user_data_gen.data_utils import generate_user_data
+else:
+    raise ValueError("Unknown implementation specified. Use 'old' or 'new'. (BE MINDFUL OF CAPITALISATION!!)")
 
 def profile_function(num_users):
-    # profiling the generate_user_data function.
     cProfile.run(f'generate_user_data({num_users})', '${PROFILE_FILE}')
-    
-    # writing profiling stats to the output file.
     with open('${TEXT_FILE}', 'w') as f:
         stats = pstats.Stats('${PROFILE_FILE}', stream=f)
         stats.strip_dirs().sort_stats('cumulative').print_stats()
@@ -30,7 +41,7 @@ if __name__ == '__main__':
     profile_function($NUM_USERS)
 EOF
 
-# removes .prof file
+# removing .prof file.
 rm -f "$PROFILE_FILE"
 
-echo "profiling complete. output saved to $TEXT_FILE."
+echo "Profiling complete. Output saved to $TEXT_FILE."
